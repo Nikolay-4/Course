cb8ff5abc57fbc06d7a441bd32dda7af8195f4ca
Nikolay-4 <ex@ex.com>
2021-11-16 23:50:58 +0400

    added registration route and event updated composer.lock

--- AuthService/app/Http/Controllers/AuthController.php
+++ AuthService/app/Http/Controllers/AuthController.php
@@ -2,7 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Registration\EmailRegisterRequest;
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Exchange\AMQPExchangeType;
+use PhpAmqpLib\Message\AMQPMessage;
 
 class AuthController extends Controller
 {
@@ -13,7 +20,7 @@ class AuthController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => ['login']]);
+        $this->middleware('auth:api', ['except' => ['login', 'registration']]);
     }
 
     /**
@@ -64,6 +71,52 @@ class AuthController extends Controller
         return $this->respondWithToken(auth()->refresh());
     }
 
+    public function registration(Request $request)
+    {
+        $input = $this->validate($request, [
+            'email' => ['required', 'email', 'unique:users'],
+            'name' => ['required', 'string', 'min:3', 'max:255'],
+            'password' => ['required', 'string', 'min:6', 'max:255'],
+        ]);
+        $user = new User();
+        $user->name = $input['name'];
+        $user->email = $input['email'];
+        $user->password = Hash::make($input['password']);
+        $user->save();
+//        event(new Registered($user));
+
+
+        $exchange = 'router';
+        $queue = 'msgs';
+
+        $connection = new AMQPStreamConnection(
+            config('queue.rabbitmq.host'),
+            config('queue.rabbitmq.port'),
+            config('queue.rabbitmq.user'),
+            config('queue.rabbitmq.password'),
+            config('queue.rabbitmq.vhost')
+        );
+        $channel = $connection->channel();
+
+        $channel->queue_declare($queue, false, true, false, false);
+
+        $channel->exchange_declare($exchange, AMQPExchangeType::DIRECT, false, true, false);
+
+        $channel->queue_bind($queue, $exchange);
+
+        $messageBody = [
+            'name' => 'user created',
+            'data' => $user
+        ];
+        $message = new AMQPMessage(json_encode($messageBody), array('content_type' => 'text/plain', 'delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT));
+        $channel->basic_publish($message, $exchange);
+
+        $channel->close();
+        $connection->close();
+
+        return response()->json(['message' => 'Письмо с подтверждением было отправлено на ваш почтовый ящик'], 201);
+    }
+
     /**
      * Get the token array structure.
      *
@@ -79,4 +132,6 @@ class AuthController extends Controller
             'expires_in' => auth()->factory()->getTTL() * 60
         ]);
     }
+
+
 }
--- AuthService/routes/api.php
+++ AuthService/routes/api.php
@@ -21,6 +21,8 @@ Route::group([
     Route::post('login', [AuthController::class, 'login']);
     Route::post('logout', [AuthController::class, 'logout']);
     Route::post('refresh', [AuthController::class, 'refresh']);
-    Route::post('me', [AuthController::class, 'me']);
+    Route::get('me', [AuthController::class, 'me']);
+    Route::post('registration', [AuthController::class, 'registration']);
+
 });
 
